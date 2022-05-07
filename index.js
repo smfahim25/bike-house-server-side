2fef6f63247886a6d3764253fa6419b122817359
smfahim25 <smfahimhossen@gmail.com>
2022-05-07 17:59:03 +0600

    code added

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
 const express = require('express');
+var jwt = require('jsonwebtoken');
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
 const port = process.env.PORT || 5000;
-
-
-
-const app = express();
-
-// middleware
-
-app.use(cors());
-app.use(express.json());
-
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ortxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('coonecte')
-    // perform actions on the collection object
-    client.close();
-});
-
-app.get('/', (req, res) => {
-    res.send('ddd');
-
-});
-app.listen(port, () => {
-
-    console.log('Listening om Port', port);
-
-});
+require('dotenv').config()
