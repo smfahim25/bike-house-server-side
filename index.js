1552ce9ddefd15a27a079841c9f59f6943ee45c0
smfahim25 <smfahimhossen@gmail.com>
2022-05-08 22:46:05 +0600

    my bike

--- index.js
+++ index.js
@@ -33,9 +33,7 @@ function verifyJWT(req, res, next) {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ortxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-
-// Getting all card 
+// Getting all
 
 async function run() {
     try {
