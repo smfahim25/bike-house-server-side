dc87256b29263a76d78d78cf54ad14382abe3940
smfahim25 <smfahimhossen@gmail.com>
2022-05-08 22:38:06 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,7 +1,137 @@
 const express = require('express');
-var jwt = require('jsonwebtoken');
-const app = express()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//token verification
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
+// database connection 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ortxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+// Getting all card 
+
+async function run() {
+    try {
+        await client.connect();
+        const bikeCollection = client.db('bike-house').collection('bikes');
+
+        //ACCESS_TOKEN_SECRET
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
+        app.get('/bikes', async (req, res) => {
+            const query = {};
+            const cursor = bikeCollection.find(query);
+            const bikes = await cursor.toArray();
+            res.send(bikes);
+        });
+
+        // Getting sigle bikes 
+        app.get('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const bikes = await bikeCollection.findOne(query);
+            res.send(bikes);
+        });
+
+        // POST api or adding a new bike
+        app.post('/bikes', async (req, res) => {
+            const newBikes = req.body;
+            const result = await bikeCollection.insertOne(newBikes);
+            res.send(result);
+        });
+
+        // DELETE or removing a car
+        app.delete('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bikeCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+        // update quantity and sold
+        app.put('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedBikes = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedBikes.quantity,
+                    sold: updatedBikes.sold,
+
+                }
+            };
+            const result = await bikeCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
+        })
+
+        // my bikesAPI
+
+        app.get('/mybike', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = bikeCollection.find(query);
+                const bikes = await cursor.toArray();
+                res.send(bikes);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Bike House server is running')
+});
+
+app.listen(port, () => {
+    console.log('Bike House running on port 5000');
+})
